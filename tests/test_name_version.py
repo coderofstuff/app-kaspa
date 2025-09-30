28d7831b33023dad080a896f06422fa1b45ab9e7
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2025-09-30 11:59:40 +0200

    update tests according to PR remarks

--- tests/test_name_version.py
+++ tests/test_name_version.py
@@ -1,9 +1,10 @@
 from application_client.kaspa_command_sender import KaspaCommandSender
 from application_client.kaspa_response_unpacker import unpack_get_app_and_version_response
+from utils import verify_version
 
 
 # Test a specific APDU asking BOLOS (and not the app) the name and version of the current app
-def test_get_app_and_version(backend, backend_name):
+def test_get_app_and_version(backend):
     # Use the app interface instead of raw interface
     client = KaspaCommandSender(backend)
     # Send the special instruction to BOLOS
@@ -12,4 +13,4 @@ def test_get_app_and_version(backend, backend_name):
     app_name, version = unpack_get_app_and_version_response(response.data)
 
     assert app_name == "Kaspa"
-    assert version == "1.2.0"
+    verify_version(version)
--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import List
+import re
 
 from secp256k1 import PublicKey
 
@@ -7,5 +9,37 @@ ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
 # Check if a signature of a given message is valid
 def check_signature_validity(public_key: bytes, signature: bytes, sighash: bytes) -> bool:
     pkey = PublicKey(public_key, True)
-    
+
     return pkey.schnorr_verify(sighash, signature, None, True)
+
+def verify_version(version: str) -> None:
+    """Verify the app version, based on defines in Makefile
+
+    Args:
+        Version (str): Version to be checked
+    """
+
+    vers_dict = {}
+    vers_str = ""
+    lines = _read_makefile()
+    version_re = re.compile(r"^APPVERSION_(?P<part>\w)\s?=\s?(?P<val>\d*)", re.I)
+    for line in lines:
+        info = version_re.match(line)
+        if info:
+            dinfo = info.groupdict()
+            vers_dict[dinfo["part"]] = dinfo["val"]
+    try:
+        vers_str = f"{vers_dict['M']}.{vers_dict['N']}.{vers_dict['P']}"
+    except KeyError:
+        pass
+    assert version == vers_str
+
+
+def _read_makefile() -> List[str]:
+    """Read lines from the parent Makefile """
+
+    parent = Path(__file__).parent.parent.resolve()
+    makefile = f"{parent}/Makefile"
+    with open(makefile, "r", encoding="utf-8") as f_p:
+        lines = f_p.readlines()
+    return lines
